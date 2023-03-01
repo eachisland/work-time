6e0bf46a0b862199bbc6a9caccf1512d837558ff
eachisland <126067130+eachisland@users.noreply.github.com>
2023-03-01 13:26:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _6zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            if (num == 0)
+            {
+                Console.WriteLine("no");
+
+
+
+            }
+            if (num >= 1)
+            {
+                if (num <= 100)
+                {
+                    Console.WriteLine("yes");
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+               
+            }
+                if (num < 0)
+                {
+                   
+                    if (num <= -100)
+                    {
+                        Console.WriteLine("yes");
+                    }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+            
+        }
+    }
+}
